1b81a2502fa99f4b83345d4ecec43f9b18148633
David Griver <dgriver@gmail.com>
2015-07-08 15:49:02 +0300

    fixed setting file for heroku

--- sikumim/settings.py
+++ sikumim/settings.py
@@ -124,10 +124,11 @@ CKEDITOR_CONFIGS = {
 #HEROKU DEPLOYMENT
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='postgres://david:david@localhost/sikumim_db')
+
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-DEBUG = False
+DEBUG = True
