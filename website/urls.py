30c5ff1ad3059647fa927dda8bd75989b8230dfe
David Griver <dgriver@gmail.com>
2015-07-27 09:56:15 +0300

    added social registration

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^accounts/', include('allauth.urls')),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
