ffbb71135dd944ba76c2550cad0af8a5b252fa0d
David Griver <dgriver@gmail.com>
2015-08-23 19:18:34 +0300

    fixes mailgun password

--- sikumim/settings/development.py
+++ sikumim/settings/development.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 from .base import *
 
-# mailgun
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.mailgun.org'
-EMAIL_HOST_USER = 'postmaster@sikumia.co.il'
-EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
-EMAIL_PORT = 587
+
 
 # memcache
 CACHES = {
